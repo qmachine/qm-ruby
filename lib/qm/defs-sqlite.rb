07732eebb18a4ff17940b2cfc0dee3dce410aa2c
Sean Wilkinson <sean@mathbiol.org>
2015-01-31 01:22:42 -0600

    Improved SQLite data model

--- lib/qm/defs-sqlite.rb
+++ lib/qm/defs-sqlite.rb
@@ -2,7 +2,7 @@
 
 #-  defs-sqlite.rb ~~
 #                                                       ~~ (c) SRW, 16 Jul 2014
-#                                                   ~~ last updated 30 Jan 2015
+#                                                   ~~ last updated 31 Jan 2015
 
 require 'json'
 require 'sqlite3'
@@ -15,14 +15,34 @@ module QM
           # This method isn't meaningful for the current implementation because
           # the `execute` method opens and closes the SQLite database file for
           # each and every request. If you want high performance, do *NOT* use
-          # SQLite as storage for QM :-P
+          # SQLite as storage for QMachine :-P
+            return
+        end
+
+        def collect_garbage()
+          # This method needs documentation.
+            now = (1000 * Time.now.to_f).to_i
+            execute("DELETE FROM avars WHERE (exp_date < #{now})")
             return
         end
 
         def connect(opts = {})
           # This method needs documentation.
-            @db ||= opts[:sqlite] if opts.has_key?(:sqlite)
-            return @db
+            if opts.has_key?(:sqlite) then
+                @filename ||= opts[:sqlite]
+                execute <<-sql
+                    CREATE TABLE IF NOT EXISTS avars (
+                        body TEXT NOT NULL,
+                        box TEXT NOT NULL,
+                        exp_date INTEGER NOT NULL,
+                        key TEXT NOT NULL,
+                        status TEXT,
+                        PRIMARY KEY (box, key)
+                    );
+                    sql
+                collect_garbage
+            end
+            return @filename
         end
 
         def execute(query)
@@ -32,55 +52,51 @@ module QM
           # query. A caveat, of course, is that the special ":memory:" database
           # doesn't work correctly, but ":memory:" isn't *persistent* storage
           # anyway.
-            begin
-                db = SQLite3::Database.open(@db)
-                now = (1000 * Time.now.to_f).to_i
-                db.execute_batch <<-sql
-                    CREATE TABLE IF NOT EXISTS avars (
-                        body TEXT NOT NULL,
-                        box TEXT NOT NULL,
-                        exp_date INTEGER NOT NULL,
-                        key TEXT NOT NULL,
-                        status TEXT,
-                        PRIMARY KEY (box, key)
-                    );
-                    DELETE FROM avars WHERE (exp_date < #{now})
-                    sql
-              # We have to execute the query code `query` separately because
-              # the `db.execute_batch` function always returns `nil`, which
-              # prevents us from being able to retrieve the results of the
-              # query.
-                x = db.execute(query)
-            rescue SQLite3::Exception => err
-                STDERR.puts "Exception occurred: #{err} (#{query})"
-            ensure
-                db.close if db
+            done = false
+            until (done == true) do
+                begin
+                    db = SQLite3::Database.open(@filename)
+                    x = db.execute(query)
+                    done = true
+                rescue SQLite3::Exception => err
+                    if (err.is_a?(SQLite3::BusyException) == false) then
+                        STDERR.puts "Exception occurred: '#{err}':\n#{query}"
+                    end
+                ensure
+                    db.close if db
+                end
             end
             return x
         end
 
+        def exp_date()
+          # This method needs documentation.
+            return (1000 * (Time.now.to_f + @settings.avar_ttl)).to_i
+        end
+
         def get_avar(params)
           # This method needs documentation.
-            exp_date = (1000 * (Time.now.to_f + @settings.avar_ttl)).to_i
+            box, key = params[0], params[1]
             x = execute <<-sql
                 SELECT body FROM avars
-                WHERE box = '#{params[0]}' AND key = '#{params[1]}'
+                WHERE box = '#{box}' AND exp_date > #{now} AND key = '#{key}'
                 sql
             if x.length == 0 then
                 y = '{}'
             else
-                execute <<-sql
-                    UPDATE avars SET exp_date = #{exp_date}
-                    WHERE box = '#{params[0]}' and key = '#{params[1]}'
-                    sql
+              # If a row was found, update its expiration date.
                 y = x[0][0]
+                x = execute <<-sql
+                    UPDATE avars SET exp_date = #{exp_date}
+                    WHERE box = '#{box}' and key = '#{key}'
+                    sql
             end
             return y
         end
 
         def get_list(params)
           # This method needs documentation.
-            b, now, s = params[0], (1000 * Time.now.to_f).to_i, params[1]
+            b, s = params[0], params[1]
             x = execute <<-sql
                 SELECT key FROM avars
                 WHERE box = '#{b}' AND exp_date > #{now} AND status = '#{s}'
@@ -93,27 +109,24 @@ module QM
             @settings = opts
         end
 
+        def now()
+          # This method needs documentation.
+            return (1000 * Time.now.to_f).to_i
+        end
+
         def set_avar(params)
           # This method needs documentation.
             body, box, key = params.last, params[0], params[1]
-            ed = (1000 * (Time.now.to_f + @settings.avar_ttl)).to_i
-            if (params.length == 4) then
-              # This arm runs only when a client writes an avar that represents
-              # a task description.
-                status = params[2]
-                execute <<-sql
-                    INSERT OR REPLACE INTO avars
-                        (body, box, exp_date, key, status)
-                    VALUES ('#{body}', '#{box}', #{ed}, '#{key}', '#{status}')
-                    sql
-            else
-              # This arm runs when a client is writing a "regular avar".
-                execute <<-sql
-                    INSERT OR REPLACE INTO avars (body, box, exp_date, key)
-                    VALUES ('#{body}', '#{box}', #{ed}, '#{key}')
-                    sql
-            end
+            status = (params.length == 4) ? "'#{params[2]}'" : 'NULL'
+            execute <<-sql
+                INSERT OR REPLACE INTO avars (body, box, exp_date, key, status)
+                VALUES ('#{body}', '#{box}', #{exp_date}, '#{key}', #{status})
+                sql
+            collect_garbage
+            return
         end
+
+        private :collect_garbage, :execute, :exp_date, :now
 
     end
 
